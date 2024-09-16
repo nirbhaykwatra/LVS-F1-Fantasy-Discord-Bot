a7a25540e4505f13d1338570d1110301af5cfbc5
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-15 23:27:46 -0700

    Create custom logger method in settings file
    Create method to instantiate custom loggers as required.

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import pathlib
 import os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,3 +10,26 @@ CMDS_DIR = BASE_DIR/"commands"
 
 GUILD_ID = int(os.getenv('GUILD_ID'))
 TOKEN = os.getenv('TOKEN')
+
+# Create logging format for console handler
+logFormat = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}',
+                         datefmt='%Y-%m-%d %H:%M:%S',
+                         style='{')
+
+def create_logger(name: str) -> logging.Logger:
+    # Create logger object
+    logger = logging.getLogger(name)
+
+    # Set level to DEBUG for base logger object
+    logger.setLevel(logging.DEBUG)
+
+    # Create console handler object
+    console = logging.StreamHandler()
+    # Set level to INFO for console handler
+    console.setLevel(logging.INFO)
+    # Set format to logFormat for console handler
+    console.setFormatter(logFormat)
+    # Add console handler to logger
+    logger.addHandler(console)
+
+    return logger
