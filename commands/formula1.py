4e8175a091e125e58a9197fd420639400eaaf802
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-16 05:18:52 -0700

    Import FastF1 modules

---
+++ commands/formula1.py
@@ -0,0 +1,9 @@
+import discord
+from discord import app_commands
+from discord.app_commands import Choice
+from discord.ext import commands
+import settings
+import fastf1
+from fastf1.ergast import Ergast
+import pandas as pd
+
