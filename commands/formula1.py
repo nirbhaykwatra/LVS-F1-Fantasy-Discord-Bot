a151ec679490db948d327f84828737d64495662d
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-16 05:19:26 -0700

    Initialize FastF1 variables in Formula1 cog

--- commands/formula1.py
+++ commands/formula1.py
@@ -7,3 +7,9 @@ import fastf1
 from fastf1.ergast import Ergast
 import pandas as pd
 
+#region Initialize FastF1
+logger = settings.create_logger('fantasy-fastf1')
+fastf1.ergast.interface.BASE_URL = "https://api.jolpi.ca/ergast/f1"
+ergast = Ergast()
+fastf1.Cache.enable_cache(f'G:\\projects\\personal\\programming\\LVS-F1-Fantasy-Discord-Bot\\data\\fastf1\\cache')
+#endregion
