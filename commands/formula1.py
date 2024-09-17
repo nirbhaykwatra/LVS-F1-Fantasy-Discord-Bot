72547409c4f3f98acb2aeaf88bc8a25b92442310
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-16 22:10:45 -0700

    Add embeds to driver info command

--- commands/formula1.py
+++ commands/formula1.py
@@ -61,7 +61,86 @@ class Formula1(commands.Cog):
     @stats_group.command(name='driver', description='Get information about Formula 1 drivers.')
     @app_commands.choices(driver=drivers())
     async def get_driver_data(self, interaction: discord.Interaction, driver: Choice[str]):
-        await interaction.response.send_message(f'Information about {driver.name}: ', ephemeral=True)
+
+        logger.info(f"Command 'driver' executed with name {driver.name} (TLA: {driver.value})")
+
+        #region Driver Info embed
+        driver_info_embed = discord.Embed(title=driver.name,
+                                          url=wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "driverUrl"][drivers().index(driver)],
+                                          description=f'{driver.value}',
+                                          colour=discord.Colour.from_str(plotting.get_driver_color(driver.name, ff1session))
+                                          )
+
+        driver_info_embed.set_author(name=wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "constructorNames"][drivers().index(driver)][0])
+
+        driver_info_embed.add_field(name=wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "driverNumber"][drivers().index(driver)],
+                                    value="Driver Number",
+                                    inline=True)
+        driver_info_embed.add_field(name=wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "driverNationality"][drivers().index(driver)],
+                                    value="Nationality",
+                                    inline=True)
+
+        date = wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "dateOfBirth"][drivers().index(driver)].to_pydatetime()
+        driver_info_embed.add_field(name=date.strftime("%d %b %Y"),
+                                    value="Date of Birth",
+                                    inline=True)
+        driver_info_embed.add_field(name=str(wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "points"][drivers().index(driver)]),
+                                    value="Points",
+                                    inline=True)
+        driver_info_embed.add_field(name=f'P{wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "positionText"][drivers().index(driver)]}',
+                                    value="Championship Standing",
+                                    inline=True)
+        age = relativedelta(datetime.now(), date).years
+        driver_info_embed.add_field(name=age,
+                                    value="Age",
+                                    inline=True)
+
+        driver_first_name = wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "givenName"][drivers().index(driver)]
+        driver_last_name = wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "familyName"][drivers().index(driver)]
+
+        driver_profile = f"https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/{driver_first_name[0].upper()}/{driver_first_name[0:3].upper()}{driver_last_name[0:3].upper()}01_{driver_first_name}_{driver_last_name}/{driver_first_name[0:3]}{driver_last_name[0:3]}01.png"
+        logger.info(f'Driver URL: {driver_profile}')
+
+        driver_info_embed.set_image(
+            url=f"https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers"
+                f"/{driver_first_name[0].upper()}/{driver_first_name[0:3].upper()}{driver_last_name[0:3].upper()}01"
+                f"_{driver_first_name}_{driver_last_name}/{driver_first_name[0:3]}{driver_last_name[0:3]}01.png")
+        #endregion
+
+        #region Driver Statistics embed
+        driver_stats_embed = discord.Embed(title=f"{str(wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "points"][drivers().index(driver)])} Points",
+                                          colour=discord.Colour.from_str(
+                                              plotting.get_driver_color(driver.value, ff1session))
+                                          )
+
+        driver_stats_embed.set_author(name=f"{driver.name}'s Season Summary")
+
+        driver_stats_embed.add_field(name=wdc_standings.loc[wdc_standings["driverCode"] == driver.value, "wins"][drivers().index(driver)],
+                                    value="Wins",
+                                    inline=True)
+
+        driver_stats_embed.add_field(name="Podiums",
+                                    value=f'',
+                                    inline=True)
+
+        driver_stats_embed.add_field(name="Championship Standing",
+                                    value=f'',
+                                    inline=True)
+
+        driver_stats_embed.add_field(name="Against Teammate (Race)",
+                                    value=f'',
+                                    inline=True)
+
+        driver_stats_embed.add_field(name="Against Teammate (Qualifying)",
+                                    value=f'',
+                                    inline=True)
+        driver_stats_embed.add_field(name="",
+                                    value=f'Constructor',
+                                    inline=True)
+        #endregion
+
+        await interaction.response.defer(ephemeral=True)
+        await interaction.followup.send(f'', embeds=[driver_info_embed, driver_stats_embed], ephemeral=True)
 
     @stats_group.command(name='grand-prix', description='Get information about Formula 1 Grand Prix events.')
     async def get_grand_prix_data(self, interaction: discord.Interaction):
