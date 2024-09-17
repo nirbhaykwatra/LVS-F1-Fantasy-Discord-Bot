902551e3781c8197e03c95edeca7530c2194eac2
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2024-09-17 05:14:38 -0700

    Create driver stats library
    Created module to calculate driver stats

---
+++ utilities/drstatslib.py
@@ -0,0 +1,3 @@
+# A library for calculating driver statistics requiring more data manipulation than usual.
+# For example, calculating driver podiums, teammate battles, positions gained/lost, etc.
+
