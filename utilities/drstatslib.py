bea8ee789cf5c6d6e43bcc20c2e08060d768ba9a
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2024-09-18 20:09:21 -0700

    Create basic method to calculate podiums using Ergast data

--- utilities/drstatslib.py
+++ utilities/drstatslib.py
@@ -4,3 +4,27 @@ import settings
 from utilities import fastf1util as f1
 
 logger = settings.create_logger('driver-stats')
+
+# Get the number of podium positions the driver has been in.
+def get_driver_podiums(driver: str) -> int:
+    podiums = 0
+    rounds = len(f1.ergast.get_race_schedule(settings.F1_SEASON)['round'])
+    for round in range(1, rounds):
+        try:
+            result = f1.ergast.get_race_results(settings.F1_SEASON, round).content[0]
+        except IndexError as e:
+            result = None
+            logger.warning(f'Round {round} has no results.')
+
+        if result is not None:
+
+            position = result.loc[result['driverCode'] == driver, 'position'].item()
+
+            if position <= 3:
+                podiums += 1
+
+            logger.info(f'Results for round {round}: {position}')
+
+
+    return podiums
+
