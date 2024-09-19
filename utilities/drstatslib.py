672c297b48bacd54323a73b634681d5388935c16
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2024-09-18 20:08:43 -0700

    import fastf1util to drstatslib

--- utilities/drstatslib.py
+++ utilities/drstatslib.py
@@ -1,3 +1,6 @@
 # A library for calculating driver statistics requiring more data manipulation than usual.
 # For example, calculating driver podiums, teammate battles, positions gained/lost, etc.
+import settings
+from utilities import fastf1util as f1
 
+logger = settings.create_logger('driver-stats')
