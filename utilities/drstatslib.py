601d50b44697a1430d5a6ca4932903afc9a1319a
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-21 10:06:43 -0700

    Create per-race stats method in drstatslib

--- utilities/drstatslib.py
+++ utilities/drstatslib.py
@@ -2,9 +2,11 @@
 # For example, calculating driver podiums, teammate battles, positions gained/lost, etc.
 import settings
 from utilities import fastf1util as f1
+from utilities import postgresql as sql
 
 logger = settings.create_logger('driver-stats')
 
+#region Retroactive methods
 # Get the number of podium positions the driver has been in.
 def get_driver_podiums(driver: str) -> int:
     podiums = 0
@@ -18,16 +20,21 @@ def get_driver_podiums(driver: str) -> int:
 
         if result is not None:
 
-            position = result.loc[result['driverCode'] == driver, 'position'].item()
+            try:
+                position = result.loc[result['driverCode'] == driver, 'position'].item()
 
-            if position <= 3:
-                podiums += 1
+                if position <= 3:
+                    podiums += 1
+            except ValueError or UnboundLocalError as e:
+                pass
 
-            logger.info(f'Results for round {round}: {position}')
+            try:
+                logger.info(f'{driver} position in round {round}: {position}')
+            except UnboundLocalError as e:
+                logger.warning(f"Could not find driver {driver} in round {round}.")
 
 
     return podiums
-
 # Get either the qualifying or race teammate battle between a driver and their teammate as a ratio.
 def get_driver_teammate_battle(driver: str, season: int) -> str:
 
@@ -41,8 +48,45 @@ def get_driver_position_delta(driver: str) -> [int]:
     position_delta = [0, 0]
 
     return position_delta
+#endregion
+
+#region Per-Race methods
+
+def calculate_driver_stats(driver: str, round: int):
+    if did_driver_podium(driver, round):
+        sql.drivers.loc[sql.drivers['driverCode'] == driver, 'podiums'] += 1
+        # Here, add 1 to the podium value of the drivers DataFrame, imported from the database at launch
+        pass
+
+def did_driver_podium(driver: str, round: int) -> bool:
+    try:
+        result = f1.ergast.get_race_results(settings.F1_SEASON, round).content[0]
+    except IndexError as e:
+        result = None
+        logger.warning(f'Round {round} has no results yet. Try again later!')
+        return False
+
+    if result is not None:
+        try:
+            position = result.loc[result['driverCode'] == driver, 'position'].item()
+
+            if position <= 3:
+                return True
+        except ValueError or UnboundLocalError as e:
+            pass
+
+        try:
+            logger.info(f'{driver} position in round {round}: {position}')
+        except UnboundLocalError as e:
+            logger.warning(f"Could not find driver {driver} in round {round}.")
+
+
+def calculate_teammate_battle(driver: str):
+    pass
+
+#endregion
 
 if __name__ == "__main__":
-    logger.info(f'Podiums: {get_driver_podiums("VER")}')
+    logger.info(f'Podiums: {get_driver_podiums("COL")}')
     logger.info(f'Battle: {get_driver_teammate_battle("VER", settings.F1_SEASON)}')
     logger.info(f'Position delta: {get_driver_position_delta("VER")}')
