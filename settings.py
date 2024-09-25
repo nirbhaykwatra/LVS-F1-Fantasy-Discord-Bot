367a9d78d7e7168a140462fc085701c84e31c4fa
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-25 02:36:04 -0700

    Add fantasy data constants to settings

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 #region Fantasy Data
 F1_SEASON = 2024
-F1_ROUND = 18
+F1_ROUND = 19
 #endregion
 
 #region Constants
@@ -17,17 +17,28 @@ FASTF1_CACHE_DIR = BASE_DIR/"data"/"fastf1"/"cache"
 
 F1_IMAGE_BASE_URL = "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/"
 POSTGRES_BASE_URL = f'postgresql://{os.getenv('SQLUSER')}:{os.getenv('SQLPASS')}@{os.getenv('SQLHOST')}/fantasy_{F1_SEASON}'
+POSTGRES_PLAYER_BASE_URL = f'postgresql://{os.getenv('SQLUSER')}:{os.getenv('SQLPASS')}@{os.getenv('SQLHOST')}/players_{F1_SEASON}'
 
+RACE_POINTS = [22, 18, 15, 12, 10, 8, 6, 4, 2, 1]
+SPRINT_POINTS = [5, 4, 3, 2, 1, -1, -2, -3, -4, -5]
+CONSTRUCTOR_POINTS = [5, 4, 3, 2, 1]
+WILDCARD_PTS_RACE = 3
+WILDCARD_PTS_QUALI = 2
+WILDCARD_PTS_RACE_SPRINT = 2
+WILDCARD_PTS_QUALI_SPRINT = 1
 
 GUILD_ID = int(os.getenv('GUILD_ID'))
 TOKEN = os.getenv('TOKEN')
 #endregion
+
+EMBED_COLOR = 0xe8272a
 
 # Create logging format for console handler
 logFormat = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}',
                          datefmt='%Y-%m-%d %H:%M:%S',
                          style='{')
 
+#TODO: Output logs to logs directory
 def create_logger(name: str) -> logging.Logger:
     # Create logger object
     logger = logging.getLogger(name)
