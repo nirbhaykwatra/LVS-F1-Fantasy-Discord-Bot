576d00a9a8d78958ad25755397ec72164f9d60de
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2024-09-25 02:37:57 -0700

    Create timing.py

---
+++ utilities/timing.py
@@ -0,0 +1,4 @@
+# TODO: Implement automatic round changing and timezone conversions for all relevant
+#   information methods such as grand-prix info, team and draft.
+
+# TODO: Implement draft deadlines and draft freeze windows
