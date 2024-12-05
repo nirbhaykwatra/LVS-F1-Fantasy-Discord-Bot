762681298c502b6a110fac72791a0162c70f72da
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2024-12-05 09:10:24 +0530

    Create command to add F1 events to server calendar

--- commands/fantasysetup.py
+++ commands/fantasysetup.py
@@ -23,14 +23,46 @@ class FantasySetup(commands.Cog):
     @app_commands.checks.has_role('Administrator')
     async def add_season_events(self, interaction: discord.Interaction):
 
+        await interaction.response.defer()
+
+        guild = self.bot.get_guild(settings.GUILD_ID)
+        season = f1.event_schedule
+
+        if guild is not None:
+
+            for event in range(24, len(season['RoundNumber'])):
+                logger.info(f"Round {event} ----------------------------------------------------------------------")
+
+                name = season['OfficialEventName'][event]
+                logger.info(f"Event Name: {name}")
+
+                start_time: dt.pd.Timestamp = season['Session5Date'][event]
+                logger.info(f"Event Start Time: {start_time}")
+
+                end_time = start_time + dt.timedelta(hours=3)
+                logger.info(f"Event End Time: {end_time}")
+
+                location = season['Location'][event]
+                logger.info(f"Event Location: {location}")
+
+
+                await guild.create_scheduled_event(
+                    name=name,
+                    start_time=start_time,
+                    end_time=end_time,
+                    location=location,
+                    entity_type=discord.EntityType.external,
+                    privacy_level=discord.PrivacyLevel.guild_only,
+                )
+
         embed = discord.Embed(
             title='Season calendar has been added.',
-            description=''
+            description='',
+            colour=settings.EMBED_COLOR
         )
-
         embed.set_author(name="Setup")
 
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.followup.send(embed=embed, ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
