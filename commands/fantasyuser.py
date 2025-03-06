bb255999a0937e7b90f48c6b35832205297dfe3a
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-03-06 09:58:15 -0800

    Add points breakdown, points table user command and create drafting checks

--- commands/fantasyuser.py
+++ commands/fantasyuser.py
@@ -1,13 +1,16 @@
+import json
 import discord
 from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import commands
+
+
 import settings
 from utilities import datautils as dt
 from utilities import postgresql as sql
 from utilities import fastf1util as f1
 import pandas as pd
-import dataframe_image as dfi
+import df2img as df2
 import utilities.timing as timing
 
 logger = settings.create_logger('fantasy-user')
@@ -22,9 +25,26 @@ class FantasyUser(commands.Cog):
     @app_commands.choices(timezone=dt.timezone_choice_list())
     async def register(self, interaction: discord.Interaction,  timezone: dt.Choice[str], team_name: str, team_motto: str = "The one and only!"):
 
-        if any(sql.players.userid == interaction.user.id):
-            logger.error(f'User {interaction.user.name} with {interaction.user.id} already registered.')
-            await interaction.response.send_message(f'{interaction.user.name} is already registered!', ephemeral=True)
+        try:
+            if not any(sql.players.userid == interaction.user.id):
+
+                unregistered_embed = discord.Embed(
+                    title=f"You are not registered!",
+                    description=f" Please register to draft!",
+                    colour=settings.EMBED_COLOR
+                )
+
+                await interaction.followup.send(embed=unregistered_embed, ephemeral=True)
+                return
+        except ValueError as e:
+
+            unregistered_embed = discord.Embed(
+                title=f"You are not registered!",
+                description=f" Please register to draft!",
+                colour=settings.EMBED_COLOR
+            )
+
+            await interaction.followup.send(embed=unregistered_embed, ephemeral=True)
             return
 
         await interaction.response.defer(ephemeral=True)
@@ -33,7 +53,8 @@ class FantasyUser(commands.Cog):
         player_record = pd.Series({'username': interaction.user.name,
                          'userid': interaction.user.id,
                          'teamname': f"{team_name}",
-                         'teammotto': f"{team_motto}",
+                         'teammotto': f"{team_motto}", 
+                         'points': 0,
                          'timezone': timezone.value
                                    })
         results_record = pd.Series({'userid': interaction.user.id,
@@ -42,6 +63,11 @@ class FantasyUser(commands.Cog):
         #endregion
 
         sql.players = pd.concat([sql.players, player_record.to_frame().T], ignore_index=True)
+        sql.results = pd.concat([sql.results, results_record.to_frame().T], ignore_index=True)
+        
+        for event in range(0, len(f1.event_schedule.RoundNumber.to_list())):
+            sql.results.loc[sql.results['userid'] == interaction.user.id, f'round{event + 1}'] = 0
+        
         logger.info(f"Registered player {interaction.user.name}.")
 
         #region Player Profile embed
@@ -75,6 +101,7 @@ class FantasyUser(commands.Cog):
         #endregion
         sql.create_player_table(interaction.user.id)
         sql.write_to_fantasy_database('players', sql.players)
+        sql.write_to_fantasy_database('results', sql.results)
         sql.import_players_table()
 
         await interaction.followup.send(f'Registered player {interaction.user.name}!', embed=embed, ephemeral=True)
@@ -95,7 +122,6 @@ class FantasyUser(commands.Cog):
                     team: Choice[str]):
 
         await interaction.response.defer(ephemeral=True)
-        # TODO: Implement exhaustion, implement response if user is not registered
 
         try:
             if not any(sql.players.userid == interaction.user.id):
@@ -123,21 +149,19 @@ class FantasyUser(commands.Cog):
                             value=f1.event_schedule.loc[f1.event_schedule['RoundNumber'] == settings.F1_ROUND, "RoundNumber"].item()
                             )
         
-        fantasy_user_table = sql.players
-        
-        #region Draft Checks
-        #TODO: Implement bogey driver check (bogey driver has to be from bottom 5 teams) before round 2, when
-        # constructor standings are available
-
+        # Deadline check
         embed_deadline = discord.Embed(title="The draft deadline has passed!", description="In case of extraordinary circumstances, contact the league administrator to see if they can "
                                                                                            "draft your team for you.", colour=settings.EMBED_COLOR)
         
-        # Deadline check
-        if timing.draft_deadline_passed(sql.players.loc[sql.players['userid'] == interaction.user.id, 'timezone'].item()):
-            await interaction.followup.send(embed=embed_deadline, ephemeral=True)
-            return
+        bDraftDeadlinePassed = timing.draft_deadline_passed(sql.players.loc[sql.players['userid'] == interaction.user.id, 'timezone'].item())
         
-        #Check for exhaustion
+        if bDraftDeadlinePassed:
+            await interaction.followup.send(embed=embed_deadline, ephemeral=True)
+            return 
+            
+        #region Draft Checks
+        
+        # Exhausted Check
         player_table = sql.retrieve_player_table(interaction.user.id)
 
         last_team = player_table[player_table['round'] == settings.F1_ROUND - 1].squeeze()
@@ -145,23 +169,51 @@ class FantasyUser(commands.Cog):
 
         common = pd.Series(list(set(last_team).intersection(set(second_last_team))))
         
+        driver_info = f1.get_driver_info(season='current')
+        
         exhausted = []
-        embed = discord.Embed(title="Invalid Draft!", description="One or more of the following picks are exhausted!", colour=settings.EMBED_COLOR)
+        embed_exhausted = discord.Embed(title="Invalid Draft!", description="One or more of the following picks are exhausted!", colour=settings.EMBED_COLOR)
         
         for element in common:
-            if driver1.value == element or driver2.value == element or driver3.value == element or bogey_driver.value == element or team.value == element:
-                embed.add_field(name=f"{element}", value="Exhausted!", inline=False)
+            if driver1.value == element or driver2.value == element or driver3.value == element or bogey_driver.value == element:
+                embed_exhausted.add_field(name=f"{driver_info.loc[driver_info['driverCode'] == element, 'givenName'].item()} "
+                                               f"{driver_info.loc[driver_info['driverCode'] == element, 'familyName'].item()}", value="Exhausted!", inline=False)
                 exhausted.append(element)
                 
-        if len(exhausted) != 0:
-            await interaction.followup.send(embed=embed, ephemeral=True)
-            return
+            if team.value == element:
+                embed_exhausted.add_field(name=f"{dt.team_names_full[element]}", value="Exhausted!", inline=False)
+                exhausted.append(element)
+                
+        bPickExhausted = len(exhausted) != 0
         
-        #Check if more than 2 drivers are in the same team
-        
-        selected_drivers = [driver1.value, driver2.value, driver3.value, bogey_driver.value]
-        
+        if bPickExhausted:
+            await interaction.followup.send(embed=embed_exhausted, ephemeral=True)
+
+        # Bogey Driver Check
         driver_info = f1.get_driver_info(settings.F1_SEASON)
+        try:
+            constructor_standings = f1.ergast.get_constructor_standings(season='current').content[0]
+        except IndexError as e:
+            constructor_standings = f1.ergast.get_constructor_standings(season=settings.F1_SEASON - 1).content[0]
+            logger.warning(
+                    f"Unable to retrieve driver standings for the year {settings.F1_SEASON}! Retrieved driver standings for the year {settings.F1_SEASON - 1} instead.")
+        
+        constructor_list = constructor_standings.constructorId.to_list()
+        
+        last_five_constructors = constructor_list[5:]
+        
+        bogey_id = driver_info.loc[driver_info['driverCode'] == bogey_driver.value, ['driverId']].squeeze()
+        bogey_constructor = f1.ergast.get_constructor_info(season='current', driver=bogey_id).constructorId.squeeze()
+
+        embed_bogey = discord.Embed(title="Invalid Draft!", description="Your bogey driver must be from the last 5 constructors.", colour=settings.EMBED_COLOR)
+        
+        bBogeyDriverTeamInvalid = bogey_constructor not in last_five_constructors
+        
+        if bBogeyDriverTeamInvalid:
+            await interaction.followup.send(embed=embed_bogey, ephemeral=True)
+
+        #Check if more than 2 drivers are in the same team
+        selected_drivers = [driver1.value, driver2.value, driver3.value, bogey_driver.value]
         
         driverIds = []
         selected_constructors = []
@@ -172,24 +224,25 @@ class FantasyUser(commands.Cog):
         for driver in driverIds:
             selected_constructors.append(f1.ergast.get_constructor_info(season='current', driver=driver).constructorId.squeeze())
             
-        bHasDuplicateConstructor = len(set(selected_constructors)) < 3
-        logger.info(f"Unique constructors: {len(set(selected_constructors))}")
-
         embed_const = discord.Embed(title="Invalid Draft!", description="You cannot pick both drivers from multiple constructors!", colour=settings.EMBED_COLOR)
+        embed_team = discord.Embed(title="Invalid Draft!", description="At least one picked driver has to represent your selected constructor!", colour=settings.EMBED_COLOR)
+        
+        bHasDuplicateConstructor = len(set(selected_constructors)) < 3
         
         if bHasDuplicateConstructor:
             await interaction.followup.send(embed=embed_const, ephemeral=True)
-            return
         
         bNoDriverFromConstructor = team.value not in selected_constructors
 
-        embed_team = discord.Embed(title="Invalid Draft!", description="At least one picked driver has to represent your selected constructor!", colour=settings.EMBED_COLOR)
-        
         if bNoDriverFromConstructor:
             await interaction.followup.send(embed=embed_team, ephemeral=True)
-            return
         
         #endregion
+        
+        bDraftInvalid = bBogeyDriverTeamInvalid or bNoDriverFromConstructor or bPickExhausted or bHasDuplicateConstructor
+        
+        if bDraftInvalid:
+            return 
         
         # Operation to modify the database table has to be done before creating the embed, as the embed will error out
         # if there are no values in the table
@@ -423,11 +476,95 @@ class FantasyUser(commands.Cog):
     async def leaderboard(self, interaction: discord.Interaction):
         await interaction.response.send_message(f'leaderboard command triggered', ephemeral=True)
 
+    @app_commands.command(name='points-breakdown', description='View points breakdown for given grand prix.')
+    @app_commands.guilds(discord.Object(id=settings.GUILD_ID))
+    @app_commands.choices(grand_prix=dt.grand_prix_choice_list())
+    async def points_breakdown(self, interaction: discord.Interaction, grand_prix: Choice[str]):
+        await interaction.response.defer(ephemeral=True)
+        try:
+            if not any(sql.players.userid == interaction.user.id):
+
+                unregistered_embed = discord.Embed(
+                    title=f"You are not registered!",
+                    description=f" Please register to view points!",
+                    colour=settings.EMBED_COLOR
+                )
+
+                await interaction.followup.send(embed=unregistered_embed, ephemeral=True)
+                return
+        except ValueError as e:
+
+            unregistered_embed = discord.Embed(
+                title=f"You are not registered!",
+                description=f" Please register to view points!",
+                colour=settings.EMBED_COLOR
+            )
+
+            await interaction.followup.send(embed=unregistered_embed, ephemeral=True)
+            return
+        
+        embed_points = discord.Embed(
+            title=f"Points Breakdown for the {grand_prix.name}",
+            colour=settings.EMBED_COLOR
+        )
+        
+        embed_points.set_author(name=f"Round {grand_prix.value}")
+        
+        embed_points.add_field(name=f"{sql.results.loc[sql.results['userid'] == interaction.user.id, f'round{grand_prix.value}'].item()} points", value=f"Total Points")
+        
+        breakdown_json = sql.results.loc[sql.results['userid'] == interaction.user.id, f'round{grand_prix.value}breakdown'].item()
+        breakdown = json.loads(breakdown_json)
+        
+        for key, value in breakdown.items():
+            embed_points.add_field(name=f"{value} points", value=f"{dt.points_breakdown_map[key]}")
+            
+        await interaction.followup.send(embed=embed_points, ephemeral=True)
+
     @app_commands.command(name='points-table', description='View the points table.')
     @app_commands.guilds(discord.Object(id=settings.GUILD_ID))
     async def points_table(self, interaction: discord.Interaction):
-        dfi.export(sql.results, 'results.png', table_conversion="chrome")
-        await interaction.response.send_message(f'points-table command triggered', ephemeral=True)
+        results_prep = sql.results.drop(axis=1, columns=['userid', 
+                                                         'round1breakdown', 'round2breakdown', 'round3breakdown',
+                                                         'round4breakdown', 'round5breakdown', 'round6breakdown',
+                                                         'round7breakdown', 'round8breakdown', 'round9breakdown',
+                                                         'round10breakdown', 'round11breakdown', 'round12breakdown',
+                                                         'round13breakdown', 'round14breakdown', 'round15breakdown',
+                                                         'round16breakdown', 'round17breakdown', 'round18breakdown',
+                                                         'round19breakdown', 'round20breakdown', 'round21breakdown',
+                                                         'round22breakdown', 'round23breakdown', 'round24breakdown'
+                                                         ])
+        results = df2.plot_dataframe(results_prep, fig_size=(2560, 600), print_index=False,
+                                     title={
+                                         "automargin": True,
+                                         "yref": "container",
+                                         "text": "LVS Formula 1 League",
+                                         "font_family": "Formula1 Display",
+                                         "font_size": 30,
+                                         "font_color": "white",
+                                         "pad_l": 1000,
+                                         "pad_t": 5
+                                     },
+                                     tbl_header={
+                                         "font_family": "Formula1 Display",
+                                         "font_color": "white",
+                                         "fill_color": "rgba(26, 26, 26, 1)",
+                                         "font_size": 10,
+                                         "font_textcase": "upper",
+                                         "line_color": "rgba(26, 26, 26, 1)",
+                                         "height": 10
+                                     },
+                                     tbl_cells={
+                                         "font_family": "Formula1 Display",
+                                         "font_color": "white",
+                                         "fill_color": "rgba(26, 26, 26, 1)",
+                                         "font_size": 12,
+                                         "font_textcase": "upper",
+                                         "line_color": "rgba(26, 26, 26, 1)",
+                                     },
+                                     paper_bgcolor="rgba(26, 26, 26, 1)"
+                                     )
+        df2.save_dataframe(fig=results, filename=settings.BASE_DIR/"data"/"points_table.png")
+        await interaction.response.send_message(file=discord.File(f'{settings.BASE_DIR}/data/points_table.png'), ephemeral=True)
     #endregion
 
     @app_commands.command(name='edit-motto', description='Edit your team motto.')
@@ -440,12 +577,35 @@ class FantasyUser(commands.Cog):
     async def edit_team_name(self, interaction: discord.Interaction):
         await interaction.response.send_message(f'Team name changed.', ephemeral=True)
 
+    @app_commands.command(name='edit-timezone', description='Edit your timezone. Make sure to enter a valid pytz timezone.')
+    @app_commands.guilds(discord.Object(id=settings.GUILD_ID))
+    async def edit_timezone(self, interaction: discord.Interaction, timezone: str):
+        
+        await interaction.response.defer(ephemeral=True)
+        
+        all_tz = dt.all_tz
+        
+        previous_timezone = sql.players.loc[sql.players['userid'] == interaction.user.id, 'timezone'].item()
+        current_timezone = timezone
+
+        embed = discord.Embed(title=f"Invalid Timezone!", description=f"Please make sure to enter a valid pytz timezone! Go here https://tinyurl.com/mspkxsua to see if your timezone is valid.", colour=settings.EMBED_COLOR)
+        
+        if current_timezone not in all_tz:
+            await interaction.followup.send(embed=embed, ephemeral=True)
+            return 
+        
+        sql.players.loc[sql.players['userid'] == interaction.user.id, 'timezone'] = current_timezone
+        sql.write_to_fantasy_database('players', sql.players)
+        sql.import_players_table()
+        
+        embed = discord.Embed(title=f"Timezone changed!", description=f"Your timezone has been changed from {previous_timezone} to {current_timezone}!", colour=settings.EMBED_COLOR)
+        
+        await interaction.followup.send(embed=embed, ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(FantasyUser(bot))
 
 if __name__ == '__main__':
-    # user_points = sql.players.loc[sql.players['userid'] == 12345, 'points']
-    # logger.info(user_points.empty)
+    logger.info(f1.ergast.get_constructor_standings(season=2024).content[0].constructorId[5:].to_list())
     pass
