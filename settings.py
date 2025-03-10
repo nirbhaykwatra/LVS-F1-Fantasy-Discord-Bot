406a8824b714d284606a7f5b916b74061aefcb48
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-03-09 21:40:49 -0700

    Change bogey points allocations

--- settings.py
+++ settings.py
@@ -58,8 +58,8 @@ QUALI_POINTS = [5, 4, 3, 2, 1]
 SPRINT_POINTS = [5, 4, 3, 2, 1, -1, -2, -3, -4, -5]
 SPRINT_QUALI_POINTS = [3, 2, 1]
 CONSTRUCTOR_POINTS = [5, 4, 3, 2, 1]
-BOGEY_POINTS = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -0.5, -1, -1.5, -2, -2.5, -3, -3.5, -4, -4.5, -5]
-BOGEY_POINTS_SPRINT = [5, 5, 4, 4, 3, 3, 2, 2, 1, 1, -0.5, -0.5, -1, -1, -1.5, -1.5, -2, -2, -2.5, -2.5]
+BOGEY_POINTS = [0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10]
+BOGEY_POINTS_SPRINT = [0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 7]
 COUNTERPICK_LIMIT = 3
 DRIVER_BAN_LIMIT = 2
 
